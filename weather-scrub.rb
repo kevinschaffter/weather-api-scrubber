1e704dea0373c91fe0c2ccea932f264e15414775
Kevin Schaffter <kevinschaffter@gmail.com>
2017-11-17 15:29:00 -0500

    Corrected API URL

--- weather-scrub.rb
+++ weather-scrub.rb
@@ -1,19 +1,13 @@
 require 'active_support/all'
-require 'HTTParty'
-require 'JSON'
+require 'httparty'
+require 'json'
 
 
-url = "http://api.openweathermap.org/data/2.5/weather?q=Miami+Beach,FL&appid=c38d29204353577f04f9a8b861e3c70e"
+url = "http://api.openweathermap.org/data/2.5/weather?q=Miami+Beach&appid=c38d29204353577f04f9a8b861e3c70e"
 response = HTTParty.get(url)
 weather = JSON.parse(response.body)
 
-
-puts weather
-
 farenheit = (weather["main"]["temp"] * 9) / 5 - 459.67
-
-
-
 
 puts "Today's Date is #{DateTime.now}"
 
